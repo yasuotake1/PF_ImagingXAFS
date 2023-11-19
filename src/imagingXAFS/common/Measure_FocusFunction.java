d4f23f0ae59600904243567087a3935643132c81
Yasuo Takeichi <takeichi@ap.eng.osaka-u.ac.jp>
2023-11-19 18:02:24 +0900

    Measure_FocusFunction.java release

--- src/imagingXAFS/common/Measure_FocusFunction.java
+++ src/imagingXAFS/common/Measure_FocusFunction.java
@@ -8,9 +8,11 @@ import ij.ImageStack;
 import ij.WindowManager;
 import ij.gui.GenericDialog;
 import ij.gui.Plot;
+import ij.gui.Roi;
 import ij.plugin.PlugIn;
 import ij.plugin.filter.GaussianBlur;
 import ij.plugin.filter.RankFilters;
+import ij.plugin.frame.RoiManager;
 import ij.process.ImageConverter;
 import ij.process.ImageStatistics;
 
@@ -36,58 +38,59 @@ public class Measure_FocusFunction implements PlugIn {
 		if (gd.wasCanceled())
 			return;
 
-		ImagePlus imp = WindowManager.getImage(listStackId[gd.getNextChoiceIndex()]).crop("stack");
-		if (imp.getType() != ImagePlus.GRAY32) {
-			ImageConverter ic = new ImageConverter(imp);
-			ic.convertToGray32();
-		}
-		int method = gd.getNextChoiceIndex();
-
-		ImageStack stack = imp.getStack();
-		int slc = imp.getNSlices();
-		double[] arrX = new double[slc];
-		double[] arrY = new double[slc];
-		ImageStatistics is;
-		GaussianBlur gb = new GaussianBlur();
-		RankFilters rf = new RankFilters();
-		
-		for (int i = 0; i < slc; i++) {
+		ImagePlus impSrc = WindowManager.getImage(listStackId[gd.getNextChoiceIndex()]);
+		double[] arrX = new double[impSrc.getNSlices()];
+		for (int i = 0; i < arrX.length; i++) {
 			arrX[i] = i + 1;
 		}
-		switch (method) {
-		case 1:// Mean of Variance
-			for (int i = 0; i < slc; i++) {
-				arrX[i] = i + 1;
-				gb.blurGaussian(stack.getProcessor(i+1), radius);
-				rf.rank(stack.getProcessor(i+1), radius, RankFilters.VARIANCE);
-				is = ImageStatistics.getStatistics(stack.getProcessor(i + 1));
-				arrY[i] = is.mean;
-			}
-			break;
-		case 2:// StdDev of Variance
-			for (int i = 0; i < slc; i++) {
-				arrX[i] = i + 1;
-				gb.blurGaussian(stack.getProcessor(i+1), radius);
-				rf.rank(stack.getProcessor(i+1), radius, RankFilters.VARIANCE);
-				is = ImageStatistics.getStatistics(stack.getProcessor(i + 1));
-				arrY[i] = is.stdDev;
-			}
-			break;
-		default:// StdDev
-			for (int i = 0; i < slc; i++) {
-				arrX[i] = i + 1;
-				is = ImageStatistics.getStatistics(stack.getProcessor(i + 1));
-				arrY[i] = is.stdDev;
-			}
-			break;
-		}
-
+		ImagePlus imp;
+		int method = gd.getNextChoiceIndex();
 		Plot plot = new Plot("Focus function", "Slice number", listMethod[method]);
-		plot.setColor(colors[0], colors[0]);
-		plot.add(styleData, arrX, arrY);
+
+		RoiManager roiManager = RoiManager.getInstance();
+		Roi[] rois;
+		if (roiManager != null && roiManager.getCount() > 0) {
+			rois = roiManager.getRoisAsArray();
+		} else {
+			rois = new Roi[] { impSrc.getRoi() };
+		}
+		String legend = "";
+		for (int i = 0; i < rois.length; i++) {
+			impSrc.setRoi(rois[i]);
+			imp = impSrc.crop("stack");
+			if (imp.getType() != ImagePlus.GRAY32) {
+				ImageConverter ic = new ImageConverter(imp);
+				ic.convertToGray32();
+			}
+			plot.setColor(colors[i]);
+			plot.add(styleData, arrX, calcFocusFunction(imp.getStack(), method));
+			legend += i > 0 ? "\t" : "";
+			legend += "ROI " + String.valueOf(i + 1);
+		}
+		if (rois.length > 1) {
+			plot.setColor(Color.BLACK);
+			plot.addLegend(legend);
+		}
 		plot.show();
-//		plot.setColor(Color.black);
-//		plot.addLegend("Standard deviation of raw values\tStandard deviation of variance\tMean of variance");
 		plot.setLimitsToFit(true);
+	}
+
+	private double[] calcFocusFunction(ImageStack stack, int method) {
+		GaussianBlur gb = new GaussianBlur();
+		RankFilters rf = new RankFilters();
+		ImageStatistics is;
+		int slc = stack.size();
+		if (method > 0) {
+			for (int i = 0; i < slc; i++) {
+				gb.blurGaussian(stack.getProcessor(i + 1), radius);
+				rf.rank(stack.getProcessor(i + 1), radius, RankFilters.VARIANCE);
+			}
+		}
+		double[] arrY = new double[slc];
+		for (int i = 0; i < slc; i++) {
+			is = ImageStatistics.getStatistics(stack.getProcessor(i + 1));
+			arrY[i] = method > 1 ? is.mean : is.stdDev;
+		}
+		return arrY;
 	}
 }
