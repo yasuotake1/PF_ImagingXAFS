e7fa78a7c1c584c35a1992944a36a5732299afc6
Yasuo Takeichi <takeichi@ap.eng.osaka-u.ac.jp>
2024-12-05 17:38:25 +0900

    Integrated spectral distribution

--- src/imagingXAFS/common/Plot_IntegSpecDist.java
+++ src/imagingXAFS/common/Plot_IntegSpecDist.java
@@ -1,42 +1,13 @@
 package imagingXAFS.common;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.lang.reflect.Method;
-
 import ij.IJ;
 import ij.ImagePlus;
-import ij.ImageStack;
 import ij.WindowManager;
 import ij.gui.GenericDialog;
 import ij.gui.NewImage;
-import ij.gui.Plot;
-import ij.gui.Roi;
-import ij.gui.WaitForUserDialog;
-import ij.io.FileInfo;
-import ij.io.OpenDialog;
-import ij.measure.CurveFitter;
-import ij.measure.Measurements;
-import ij.plugin.ContrastEnhancer;
 import ij.plugin.PlugIn;
-import ij.plugin.filter.GaussianBlur;
-import ij.process.FloatProcessor;
-import ij.process.ImageProcessor;
-import ij.process.ImageStatistics;
-import imagingXAFS.nw2a_orca.Load_OrcaStack;
-import imagingXAFS.nw2a_orca.Load_SingleOrca;
-import imagingXAFS.nw2a_orca.OrcaCommon;
-import imagingXAFS.nw2a_orca.OrcaProps;
-import imagingXAFS.nw2a_ultra.*;
 
-public class ImagingXAFSTest implements PlugIn {
-
+public class Plot_IntegSpecDist implements PlugIn {
 	public void run(String arg) {
 		Integer[] listImageId = ImagingXAFSCommon.getDataIds(false);
 		String[] listImageTitle = ImagingXAFSCommon.getDataTitles(false);
