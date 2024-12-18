2c2e2659d37041896b3e41060c0b51eec63dacba
Yasuo Takeichi <takeichi@ap.eng.osaka-u.ac.jp>
2024-12-18 10:58:06 +0900

    Updated Stitching_ShowGrid for various stitching order

--- src/imagingXAFS/common/Stitching_ShowGrid.java
+++ src/imagingXAFS/common/Stitching_ShowGrid.java
@@ -15,6 +15,7 @@ public class Stitching_ShowGrid implements PlugIn {
 		gd.addNumericField("Single image height", 2304, 0);
 		gd.addSlider("Tile overlap [%]", 0, 100, 10);
 		gd.addCheckbox("Show grid index", true);
+		gd.addChoice("Grid_order", Stitching.CHOICEORDER, Stitching.CHOICEORDER[0]);
 		gd.addNumericField("Start index", 0, 0);
 		gd.showDialog();
 		if (gd.wasCanceled())
@@ -24,6 +25,7 @@ public class Stitching_ShowGrid implements PlugIn {
 		int hei = (int) gd.getNextNumber();
 		double overlap = gd.getNextNumber();
 		boolean showIndex = gd.getNextBoolean();
+		int order = gd.getNextChoiceIndex();
 		int idx = (int) gd.getNextNumber();
 
 		ImagePlus imp = WindowManager.getCurrentImage();
@@ -56,19 +58,51 @@ public class Stitching_ShowGrid implements PlugIn {
 		if (showIndex) {
 			int size = wid > hei ? hei / 4 : wid / 4;
 			Font font = new Font("SansSerif", Font.PLAIN, size);
-			posY = hei / 2 - size / 2;
-			while (true) {
-				if (posY >= heiTarget)
-					break;
-				posX = wid / 2 - size / 2;
-				while (true) {
-					if (posX >= widTarget)
-						break;
-					overlay.add(new TextRoi(posX, posY, String.valueOf(idx), font));
-					posX += wid - overlapHor;
-					idx++;
+			int cntX = (widTarget - wid / 2) / (wid - overlapHor) + 1;
+			int cntY = (heiTarget - hei / 2) / (hei - overlapVer) + 1;
+			switch (order) {
+			case 0:// Right & Down
+				for (int i = 0; i < cntY; i++) {
+					posY = (hei - overlapVer) * i + hei / 2 - size / 2;
+					for (int j = 0; j < cntX; j++) {
+						posX = (wid - overlapHor) * j + wid / 2 - size / 2;
+						overlay.add(new TextRoi(posX, posY, String.valueOf(idx), font));
+						idx++;
+					}
 				}
-				posY += hei - overlapVer;
+				break;
+			case 1:// Left & Down
+				for (int i = 0; i < cntY; i++) {
+					posY = (hei - overlapVer) * i + hei / 2 - size / 2;
+					for (int j = cntX - 1; j >= 0; j--) {
+						posX = (wid - overlapHor) * j + wid / 2 - size / 2;
+						overlay.add(new TextRoi(posX, posY, String.valueOf(idx), font));
+						idx++;
+					}
+				}
+				break;
+			case 2:// Right & Up
+				for (int i = cntY - 1; i >= 0; i--) {
+					posY = (hei - overlapVer) * i + hei / 2 - size / 2;
+					for (int j = 0; j < cntX; j++) {
+						posX = (wid - overlapHor) * j + wid / 2 - size / 2;
+						overlay.add(new TextRoi(posX, posY, String.valueOf(idx), font));
+						idx++;
+					}
+				}
+				break;
+			case 3:// Left & Up
+				for (int i = cntY - 1; i >= 0; i--) {
+					posY = (hei - overlapVer) * i + hei / 2 - size / 2;
+					for (int j = cntX - 1; j >= 0; j--) {
+						posX = (wid - overlapHor) * j + wid / 2 - size / 2;
+						overlay.add(new TextRoi(posX, posY, String.valueOf(idx), font));
+						idx++;
+					}
+				}
+				break;
+			default:
+				break;
 			}
 		}
 
